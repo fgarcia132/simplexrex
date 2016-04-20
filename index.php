44f8ea2e6f54d1066fc7eada7253a50390650f39
Francisco Garcia <ciscog11@msn.com>
2016-04-20 08:46:08 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Simplexrex.com</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience and Goals</h1>
+		<h2>1. Purpose</h2>
+		<p>I would like my website to attract clients to a simple, elegant and thorough site that is professional and polished.</p>
+		<h2>2. Audience</h2>
+		<p>Potential customers/employers that can see good clean design. An overall professional website displaying good UX, UI, etc...</p>
+		<h2>3. Goal</h2>
+		<p>To attract freelance clients in order to work and build a website for work. And/or help with finding a full time job</p>
+		<h1>Persona</h1>
+		<p>
+			Joe Tennis want's a website that advertises the tennis related equipment that is at his store. He doesn't want to sell through the website but to show his stock and services provided as well as sales and new items to buy or demo.<br />He's just trying to get people into his store because person to person interaction is what he offers over large sites like tennis-warehouse.com
+		</p>
+		<h1>Use case</h1>
+		<p>
+			Joe Tennis is looking at potential Front end developer to make a simple site that puts the emphasis on the products and customer service he provides. He would find the site while browsing at night after work. Finding my contact information and services offered on simplexrex.com
+		</p>
+	</body>
+</html>
